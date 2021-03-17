64f524c2185bd8288e4860a9e0ddf1d67143e9fb
Dharani1230 <dharanivutukuru12@gmail.com>
2021-03-17 21:00:56 +0530

    adding linux file ssh url

---
+++ linux.sh
@@ -0,0 +1,2 @@
+hi...im practicing devops
+i want to get job im devops profile
--- user.java
+++ user.java
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 im from master branch
 im from dev branch
 heeee
 echo "hello guys"
 echo " my name is Dharani"
+=======
+ echo "hello guys"
+ echo " my name is Dharani"
+>>>>>>> 1358109... intial comment
 
